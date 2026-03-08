775a44b64bb56ca709a05d466413e8dd2ea6cdda
Vedant <vedantkotharkar74@gmail.com>
2026-03-08 13:42:10 +0530

    fix vercel flask handler

--- api/index.py
+++ api/index.py
@@ -1,19 +1,4 @@
-from flask import Flask, render_template, request
-import os
+from app import app
 
-app = Flask(__name__, template_folder="../templates", static_folder="../static")
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/cart")
-def cart():
-    return render_template("cart.html")
-
-@app.route("/checkout")
-def checkout():
-    return render_template("checkout.html")
-
-def handler(environ, start_response):
-    return app(environ, start_response)
+def handler(request):
+    return app
